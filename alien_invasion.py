64b38ba1224faf4e0e464b4efe9dc92a6397a5e3
Dammy18Prog <dowoyemi@student.cscc.edu>
2025-11-17 23:08:23 -0500

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,24 +36,20 @@ class AlienInvasion:
             self.clock.tick(60)
 
     def _check_events(self):
-        """Respond to keypresses and mouse events"""
-
+        """Respond to keypresses and releases."""
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 sys.exit()
-
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
-
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                if event.key == pygame.K_UP:
+                    self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
 
     def _update_screen (self):
         """Update images on the screen, and flip to the new screen"""
