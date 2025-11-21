ae4e591336a1b77e78712fb3ce4e2d889de026ec
Dammy18Prog <dowoyemi@student.cscc.edu>
2025-11-21 15:04:56 -0500

    Create Bullet.py

---
+++ Bullet.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Bullet rectangle
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+
+        # Start bullet at the RIGHT SIDE of the ship
+        self.rect.midleft = ai_game.ship.rect.midright
+
+        # Store float x-value for smooth movement
+        self.x = float(self.rect.x)
+
+    def update(self):
+        # Move bullet horizontally to the right
+        self.x += self.settings.bullet_speed
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
